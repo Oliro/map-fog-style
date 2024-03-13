f6b43fcd56d8182659e8a0195fc5c6165b94debf
Fernando OR <fernando.or6@gmail.com>
2024-03-13 11:01:37 -0300

    feat: created initial logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as L from 'leaflet';
+import 'leaflet.heat';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'map-fog-style';
+
+export class AppComponent implements OnInit {
+
+  private map!: L.Map;
+  private heatMap!: L.HeatLayer;
+  private watchId!: number;
+
+  private coordinatesArray: any[] = [];
+
+  ngOnInit(): void {
+
+    this.createMap();
+
+  }
+
+  createMap() {
+
+    this.map = L.map('map').setView([-23.234419534508827, -45.899720000703645], 18);
+
+    const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 10,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
+
+    this.heatMap = L.heatLayer([], { radius: 25 });
+
+    tiles.addTo(this.map)
+    this.heatMap.addTo(this.map)
+
+  }
+
+  startTracking() {
+    if (navigator.geolocation) {
+      this.watchId = navigator.geolocation.watchPosition(this.updateCoordinates.bind(this));
+    } else {
+      alert("Navegador não suportado")
+    }
+  }
+
+  stopTracking() {
+    navigator.geolocation.clearWatch(this.watchId)
+  }
+
+  updateCoordinates(position: any) {
+console.log(position)
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    this.coordinatesArray.push([latitude, longitude, 1]);
+
+    this.coordinatesArray.map((path: any) => {
+      console.log('atualizando')
+      this.heatMap.addLatLng(path);
+    });
+
+    console.log(this.coordinatesArray)
+
+  }
+
+  ngOnDestroy() {
+    if (this.map) {
+      this.map.remove();
+    }
+
+  }
 }
